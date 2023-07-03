359cc9a69d9a8068858e67eca9132bc7201567fb
muchiri08 <mbogokennedy08@gmail.com>
2023-07-03 23:32:49 +0300

    protobufs

---
+++ go.mod
@@ -0,0 +1,8 @@
+module gitub.com/muchiri08/protobufs
+
+go 1.20
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
---
+++ protobufs/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	pb "gitub.com/muchiri08/protobufs/protofiles"
+)
+
+func main() {
+	p := &pb.Person{
+		Id:    1234,
+		Name:  "Muchiri K",
+		Email: "muchirikennedy08@gmail.com",
+		Phones: []*pb.Person_PhoneNumber{
+			{Number: "0788501613", Type: pb.Person_HOME},
+		},
+	}
+
+	p1 := &pb.Person{}
+	body, _ := proto.Marshal(p)
+	_ = proto.Unmarshal(body, p1)
+	fmt.Println("Original struct loaded from proto file:", p, "\n")
+	fmt.Println("Marshaled proto data: ", body, "\n")
+	fmt.Println("Unmarshaled struct: ", p1)
+}
